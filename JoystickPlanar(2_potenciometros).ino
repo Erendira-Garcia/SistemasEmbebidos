e6408b0a44d1cfd824f250cc25dd69f666b59039
Erendira-Garcia <91690704+Erendira-Garcia@users.noreply.github.com>
2025-03-30 11:13:44 -0600

    Se añade Joystick planar
    Dispositivo de entrada que controla tanto el eje X como el eje Y, creado con dos potenciómetros.

---
+++ JoystickPlanar(2_potenciometros).ino
@@ -0,0 +1,97 @@
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+
+#define PIN_ANALOGICO_X 34
+#define PIN_ANALOGICO_Y 35
+
+#define SCREEN_WIDTH 128
+#define SCREEN_HEIGHT 64
+#define OLED_RESET -1  
+#define SCREEN_ADDRESS 0x3C
+
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
+
+void dibujar_avatar(int x, int y);
+
+void setup() {
+  Serial.begin(115200);
+  Wire.begin(21, 22);  
+
+  if (!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) {  
+    Serial.println("¡Error al iniciar OLED!");
+    while (true);
+  }
+
+  display.clearDisplay();
+    
+  //dibujar_avatar(20, 54);  // Posición (x, y) del avatar
+  display.display();  
+}
+
+void loop() {
+  int valor_x = analogRead(PIN_ANALOGICO_X);
+  int valor_y = analogRead(PIN_ANALOGICO_Y);
+
+  int x = (valor_x / 4095.0) * 127;
+  //int y = (valor_y / 4095.0) * 63;
+  int y = (valor_y / 4095.0) * 47 + 30;
+
+  Serial.println("x: ");
+  Serial.println(x);
+  Serial.println("y: ");
+  Serial.println(y);
+
+  display.clearDisplay();  
+  
+  //display.setCursor(0,0);
+  display.fillCircle(123, 4, 4, SSD1306_WHITE);
+
+  // Estrellas
+  display.drawPixel(10, 4, SSD1306_WHITE);
+  display.drawPixel(23, 12, SSD1306_WHITE);
+  display.drawPixel(32, 2, SSD1306_WHITE);
+  display.drawPixel(45, 7, SSD1306_WHITE);
+  display.drawPixel(53, 1, SSD1306_WHITE);
+  display.drawPixel(65, 5, SSD1306_WHITE);
+  display.drawPixel(78, 3,  SSD1306_WHITE);
+  display.drawPixel(84, 10, SSD1306_WHITE);
+  display.drawPixel(97, 5,  SSD1306_WHITE);
+  
+  dibujar_avatar(x, y);
+  display.display();
+  delay(500); // this speeds up the simulation
+}  
+
+// Función que dibuja el avatar
+void dibujar_avatar(int x, int y) {
+  //display.clearDisplay();
+
+  int x_offset = x;
+  int y_offset = y;
+
+  // 1. Cuadrado --> Cabeza persona
+  display.fillRect(x_offset-6, y_offset-9, 4, 4, SSD1306_WHITE);
+
+  // 2. Triángulo --> Cuerpo persona
+  display.fillTriangle(x_offset-4, y_offset-5, x_offset+2, y_offset+1, x_offset-4, y_offset+1, SSD1306_WHITE);
+
+  // 3. Triángulo --> Cabeza caballo
+  display.fillTriangle(x_offset+7, y_offset-4, x_offset+12, y_offset+1, x_offset+3, y_offset+1, SSD1306_WHITE);
+
+  // 4. Rombo --> Cola Caballo
+  display.fillTriangle(x_offset-12, y_offset+2, x_offset-5, y_offset+1, x_offset-7, y_offset+4, SSD1306_WHITE);
+  display.fillTriangle(x_offset-7, y_offset+4, x_offset-13, y_offset+5, x_offset-12, y_offset+2, SSD1306_WHITE);
+
+  // 5. Rectangulo -- > Cuerpo caballo
+  display.fillRect(x_offset-5, y_offset+1, 12, 4, SSD1306_WHITE);
+
+  // 6. Triangulo --> Pata trasera
+  display.fillTriangle(x_offset-5, y_offset+5, x_offset-2, y_offset+5, x_offset-5, y_offset+9, SSD1306_WHITE);
+
+  // 7. Triangulo --> Pata delantera
+  display.fillTriangle(x_offset+3, y_offset+5, x_offset+7, y_offset+5, x_offset+7, y_offset+9, SSD1306_WHITE);
+
+  // Mostrar la figura
+  display.display();
+}
