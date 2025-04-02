b0fc27cdfcbe302629e7869fd5819bcde3c2a521
Erendira-Garcia <91690704+Erendira-Garcia@users.noreply.github.com>
2025-04-01 20:19:20 -0600

    Se modifica escenario
    Se modifica la función del escenario

--- JoystickPlanar(2_potenciometros).ino
+++ JoystickPlanar(2_potenciometros).ino
@@ -7,26 +7,25 @@
 
 #define SCREEN_WIDTH 128
 #define SCREEN_HEIGHT 64
-#define OLED_RESET -1  
+#define OLED_RESET -1
 #define SCREEN_ADDRESS 0x3C
 
 Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
 
 void dibujar_avatar(int x, int y);
+void escenario(void);
 
 void setup() {
-  Serial.begin(115200);
-  Wire.begin(21, 22);  
 
-  if (!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) {  
+  Serial.begin(115200);
+  Wire.begin(21, 22);
+
+  if (!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) {
     Serial.println("¡Error al iniciar OLED!");
     while (true);
-  }
-
-  display.clearDisplay();
-    
-  //dibujar_avatar(20, 54);  // Posición (x, y) del avatar
-  display.display();  
+  } 
+display.clearDisplay();
+display.display();
 }
 
 void loop() {
@@ -35,38 +34,25 @@ void loop() {
 
   int x = (valor_x / 4095.0) * 127;
   //int y = (valor_y / 4095.0) * 63;
-  int y = (valor_y / 4095.0) * 47 + 30;
+  int y = (valor_y / 4095.0) * 30 + 25; // Permite 55 pixeles como máximo desplazamiento vertical
 
   Serial.println("x: ");
   Serial.println(x);
   Serial.println("y: ");
   Serial.println(y);
+  display.clearDisplay();
 
-  display.clearDisplay();  
-  
-  //display.setCursor(0,0);
-  display.fillCircle(123, 4, 4, SSD1306_WHITE);
-
-  // Estrellas
-  display.drawPixel(10, 4, SSD1306_WHITE);
-  display.drawPixel(23, 12, SSD1306_WHITE);
-  display.drawPixel(32, 2, SSD1306_WHITE);
-  display.drawPixel(45, 7, SSD1306_WHITE);
-  display.drawPixel(53, 1, SSD1306_WHITE);
-  display.drawPixel(65, 5, SSD1306_WHITE);
-  display.drawPixel(78, 3,  SSD1306_WHITE);
-  display.drawPixel(84, 10, SSD1306_WHITE);
-  display.drawPixel(97, 5,  SSD1306_WHITE);
-  
+  escenario();
   dibujar_avatar(x, y);
+
   display.display();
-  delay(500); // this speeds up the simulation
-}  
+
+  delay(500);
+}
 
 // Función que dibuja el avatar
 void dibujar_avatar(int x, int y) {
-  //display.clearDisplay();
-
+  // Centro del avatar (caballo y jinete)
   int x_offset = x;
   int y_offset = y;
 
@@ -91,7 +77,29 @@ void dibujar_avatar(int x, int y) {
 
   // 7. Triangulo --> Pata delantera
   display.fillTriangle(x_offset+3, y_offset+5, x_offset+7, y_offset+5, x_offset+7, y_offset+9, SSD1306_WHITE);
-
+  
   // Mostrar la figura
   display.display();
+}
+
+void escenario(void){
+  // Luna
+  display.fillCircle(123, 4, 4, SSD1306_WHITE);
+  display.fillCircle(126, 3, 3, SSD1306_BLACK);
+
+  // Estrellas
+  display.drawPixel(9, 1, SSD1306_WHITE);
+  display.drawPixel(10, 4, SSD1306_WHITE);
+  display.drawPixel(23, 12, SSD1306_WHITE);
+  display.drawPixel(24, 1, SSD1306_WHITE);
+  display.drawPixel(32, 2, SSD1306_WHITE);
+  display.drawPixel(45, 7, SSD1306_WHITE);
+  display.drawPixel(53, 1, SSD1306_WHITE);
+  display.drawPixel(64, 6, SSD1306_WHITE);
+  display.drawPixel(65, 5, SSD1306_WHITE);
+  display.drawPixel(78, 3, SSD1306_WHITE);
+  display.drawPixel(84, 10, SSD1306_WHITE);
+  display.drawPixel(85, 7, SSD1306_WHITE);
+  display.drawPixel(97, 5, SSD1306_WHITE);
+  display.drawPixel(109, 2, SSD1306_WHITE);
 }
